5ed8db8250685e21bc7635afd324e52e0544ca89
Engr. Shehzad <77115890+ShehzadDev@users.noreply.github.com>
2021-04-15 14:28:45 +0500

    Add files via upload

---
+++ Apllication.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		cout <<"                            \n";
+		cout <<"                            \n";
+    	cout <<"   The VC Punjab University,\n";
+		cout <<"   New (Quad-e-Azam) Campus\n";
+		cout <<"   Samsani Road , Lahore\n";
+		cout <<" Subject : For University Card Attestation\n";
+		cout <<"   Most Respectfully, \n";
+		cout <<"                     It is stated that ...............................\n";
+		cout <<"   ...................................................................\n";
+		cout <<"   -------------------------------------------------------------------\n";
+		cout <<"   -------------------------------------------------------------------\n";
+		cout <<"   -------------------------------------------------------------------\n";
+		cout <<"   -------------------------------------------------------------------\n";
+		cout <<"   -----------------------------------as soon as posible\n";
+		cout <<"           I shall be grateful tom you.\n";
+		cout <<"                                                       Your Sincerely,\n";
+		cout <<"       Dated:_________                                  Shehzad Shifa \n";
+		cout <<"                                                                      \n";
+		cout <<"     Applicant Signature:_______                 VC Signature_________\a";
+}
